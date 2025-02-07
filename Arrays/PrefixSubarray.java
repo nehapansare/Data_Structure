3b426c0b566be487444701e3beb7926d137f800b
Neha <nehapansare007@gmail.com>
2025-02-07 11:42:41 +0530

    Prefix Subarray.java

---
+++ Arrays/PrefixSubarray.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class PrefixSubarray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int prefix[]=new int[5];
+        prefix[0]=arr[0];
+        for(int i=1;i<5;i++)
+        {
+         prefix[i]=prefix[i-1]+arr[i];
+        }
+        for(int i=0;i<5;i++)
+        {
+            System.out.print(prefix[i]+" ");
+        }
+    }
+    
+}
