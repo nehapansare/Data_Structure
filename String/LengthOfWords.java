d48e6d64c1ceaf5cea907d582084bb7e6b36b77d
Neha <nehapansare007@gmail.com>
2025-02-10 15:37:49 +0530

    Length Of Word.java

---
+++ String/LengthOfWords.java
@@ -0,0 +1,7 @@
+public class LengthOfWords {
+    public static void main(String[] args) {
+        String str="Mahesh";
+        int length =str.length();
+        System.out.println(length);
+    }
+}
