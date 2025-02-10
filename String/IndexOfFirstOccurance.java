3fa79cfc4e3ef058079e871b44630e51fda14aca
Neha <nehapansare007@gmail.com>
2025-02-10 15:01:41 +0530

    Index Of First occurence.java

---
+++ String/IndexOfFirstOccurance.java
@@ -0,0 +1,9 @@
+public class IndexOfFirstOccurance {
+    public static void main(String[] args) {
+        String str="Hello World ";
+        String Target= "World";
+         int result=str.indexOf(Target);
+         System.out.println(result);
+
+    }
+}
