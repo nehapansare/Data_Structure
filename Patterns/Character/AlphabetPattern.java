34baa861bab63105c06f6559a9fea65f16543ad7
Neha <nehapansare007@gmail.com>
2025-03-02 13:08:43 +0530

    Alphabet Pattern.java

---
+++ Patterns/Character/AlphabetPattern.java
@@ -0,0 +1,17 @@
+package Patterns.Character;
+
+public class AlphabetPattern {
+    public static void main(String[] args) {
+        char ch='A';
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(ch);
+            }
+           
+            System.out.println();
+            ch++;
+        }
+    }
+}
