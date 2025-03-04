61fcb8d493ea470656fa2b50a74494fa49699cd4
Neha <nehapansare007@gmail.com>
2025-03-04 10:31:30 +0530

    Palindrome Pattern.java

---
+++ Patterns/Letter/PalindromePattern.java
@@ -0,0 +1,22 @@
+package Patterns.Letter;
+
+public class PalindromePattern {
+   public static void main(String[] args) {
+    for(int i=1;i<=5;i++)
+    {
+        for(int k=1;k<=5-i;k++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(j);
+        }
+        for(int j=i-1;j>=1;j--)
+        {
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+   } 
+}
