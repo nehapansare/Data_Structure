d17190fd7aa9398983b86e12eaa3aa2e0fa0179e
Neha <nehapansare007@gmail.com>
2025-03-07 09:03:03 +0530

    Problem3.java

---
+++ Problem/Problem3.java
@@ -0,0 +1,20 @@
+package Problem;
+import java.util.*;
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter digit:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        char a1=(char) a;
+        char b1=(char) b;
+        char c1=(char) c;
+        char d1=(char) d;
+        System.out.println(a+"-"+a1);
+        System.out.println(b+"-"+b1);
+        System.out.println(c+"-"+c1);
+        System.out.println(d+"-"+d1);
+    }
+}
